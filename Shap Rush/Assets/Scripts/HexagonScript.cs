f0ff1498ab2620d0b945da9adbc1f4a30182dec5
Ricoxico <58310052+Ricoxico@users.noreply.github.com>
2020-11-27 10:55:39 +0000

    Adição de Menu Inicial

--- Shap Rush/Assets/Scripts/HexagonScript.cs
+++ Shap Rush/Assets/Scripts/HexagonScript.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class HexagonScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float shrinkSpeed = 3f;
+    public float expandSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
         rb.rotation = Random.Range(0f, 360f);
-        transform.localScale = Vector3.one * 1f;
+        transform.localScale = Vector3.one * 0.1f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale += Vector3.one * shrinkSpeed * Time.deltaTime;
+        transform.localScale += Vector3.one * expandSpeed * Time.deltaTime;
        
         if (transform.localScale.x >= 10f)
         {
